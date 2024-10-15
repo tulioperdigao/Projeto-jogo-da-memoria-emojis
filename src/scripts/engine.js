c09000d12919866cc1bdeb6a8db0e9107f5a29c2
tulioperdigao <116309232+tulioperdigao@users.noreply.github.com>
2024-10-15 12:13:06 -0300

    Criando os cards dinamicamente

---
+++ src/scripts/engine.js
@@ -0,0 +1,28 @@
+const emojis = [
+    "😊",
+    "😊",
+    "👍",
+    "👍",
+    "👌",
+    "👌",
+    "😎",
+    "😎",
+    "😡",
+    "😡",
+    "🤮",
+    "🤮",
+    "🐱",
+    "🐱",
+    "🍔",
+    "🍔"
+];
+let openCards = [];
+
+let shuffleEmojis = emojis.sort(() => (Math.random() > 0.5 ? 2 : -1));
+
+for (let i = 0; i< emojis.length; i++) {
+  let box = document.createElement("div");
+  box.className = "item";
+  box.innerHTML = shuffleEmojis[i];
+  document.querySelector(".game").appendChild(box);
+}
